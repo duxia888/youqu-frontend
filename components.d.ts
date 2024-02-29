56263e866ea197395dc690b5b1b4ad4ddd2bab9e
huang <huang.dexin.work@outlook.com>
2024-02-29 22:32:33 +0800

    feat(用户模块): 增加心动模式，获取最匹配用户

--- components.d.ts
+++ components.d.ts
@@ -33,6 +33,7 @@ declare module 'vue' {
     VanSearch: typeof import('vant/es')['Search']
     VanSkeleton: typeof import('vant/es')['Skeleton']
     VanStepper: typeof import('vant/es')['Stepper']
+    VanSwitch: typeof import('vant/es')['Switch']
     VanTab: typeof import('vant/es')['Tab']
     VanTabbar: typeof import('vant/es')['Tabbar']
     VanTabbarItem: typeof import('vant/es')['TabbarItem']
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,42 +1,80 @@
 <script setup lang="ts">
-import {onMounted, ref} from 'vue';
-import {useRoute} from "vue-router";
+import { ref, watchEffect } from 'vue';
 import myAxios from "../plugins/myAxios.ts";
 import {Toast} from "vant";
 import UserCardList from "../components/UserCardList.vue";
+import {UserType} from "../models/user";
 
-const route = useRoute();
+const isMatchMode = ref<boolean>(false);
 
 const userList = ref([]);
+const loading = ref(true);
 
-onMounted(async () => {
-  const userListData = await myAxios.get('/user/recommend', {
-    params: {
-      pageSize: 8,
-      pageNum: 1,
-    },
-  })
-      .then(function (response) {
-        console.log('/user/recommend succeed', response);
-        return response?.data?.records;
-      })
-      .catch(function (error) {
-        console.error('/user/recommend error', error);
-        Toast.fail('请求失败');
-      })
+/**
+ * 加载数据
+ */
+const loadData = async () => {
+  let userListData;
+  loading.value = true;
+  // 心动模式，根据标签匹配用户
+  if (isMatchMode.value) {
+    const num = 10;
+    userListData = await myAxios.get('/user/match', {
+      params: {
+        num,
+      },
+    })
+        .then(function (response) {
+          console.log('/user/match succeed', response);
+          return response?.data;
+        })
+        .catch(function (error) {
+          console.error('/user/match error', error);
+          Toast.fail('请求失败');
+        })
+  } else {
+    // 普通模式，直接分页查询用户
+    userListData = await myAxios.get('/user/recommend', {
+      params: {
+        pageSize: 8,
+        pageNum: 1,
+      },
+    })
+        .then(function (response) {
+          console.log('/user/recommend succeed', response);
+          return response?.data?.records;
+        })
+        .catch(function (error) {
+          console.error('/user/recommend error', error);
+          Toast.fail('请求失败');
+        })
+  }
   if (userListData) {
-    userListData.forEach(user => {
-      user.tags = JSON.parse(user.tags);
+    userListData.forEach((user: UserType) => {
+      if (user.tags) {
+        user.tags = JSON.parse(user.tags);
+      }
     })
     userList.value = userListData;
   }
+  loading.value = false;
+}
+
+//钩子，自动监听某个方法，发生变化的时候重新加载一次数据
+watchEffect(() => {
+  loadData();
 })
 
 </script>
 
 <template>
-  <user-card-list :user-list="userList" />
-  <van-empty v-if="!userList || userList.length < 1" description="数据为空" />
+  <van-cell center title="心动模式">
+    <template #right-icon>
+      <van-switch v-model="isMatchMode" size="24" />
+    </template>
+  </van-cell>
+  <user-card-list :user-list="userList" :loading="loading"/>
+  <van-empty v-if="!userList || userList.length < 1" description="数据为空"/>
 </template>
 
 <style scoped>
